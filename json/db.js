e54c09905482c62109ef264eb27fbf323b7d8c39
noam3j <noam3jacobson@gmail.com>
2016-09-24 15:53:03 -0400

    I changed tings

--- json/db.js
+++ json/db.js
@@ -1,10 +1,24 @@
 var database = {
     
-    "companies": {[
+    "companies": [
         {
-        "name": "HTML Tutorial",
-        "longitude": "4",
-        "latitude": "5"
+            "name": "HTML Tutorial",
+            "longitude": "4",
+            "latitude": "5"
         }
-    ]},
+    ],
+    
+    "users": [
+        {
+            "name": "Noam Jacobson",
+            "email": "noam3jacobson@gmail.com",
+            "password": "noamj"
+        },
+        {
+            "name": "Carter Cook",
+            "email": "carter@gmail.com",
+            "password": "cart"
+        }
+    ]
+    
 }
--- json/dbManager.js
+++ json/dbManager.js
@@ -2,13 +2,13 @@ function getDatabase() {
     return database;
 }
 
-function getNearbyCompanies () {
+function getCompanies () {
     var companies = getDatabase().companies;
-    
-    for(i = 0; i < arr.length; i++) {
-        out += '<a href="' + arr[i].url + '">' +
-        arr[i].display + '</a><br>';
-    }
-    
-    document.getElementById("id01").innerHTML = out;
+    return companies;
 }
+
+function getUsers() {
+    var users = getDatabase().users;
+    return users;
+}
+
