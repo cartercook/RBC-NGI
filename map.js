9f07dbb005ba291cba186c9973a47a9866227d7a
Katie Yang <13hy22@queensu.ca>
2016-09-24 16:39:16 -0400

    update

--- map.js
+++ map.js
@@ -8,3 +8,10 @@ window.onload = function() {
   navigator.geolocation.getCurrentPosition(geoSuccess);
     
 };
+var map;
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
