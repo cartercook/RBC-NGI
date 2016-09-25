97fbd8408763234c79febb5f68154ca050f262ce
Katie Yang <13hy22@queensu.ca>
2016-09-25 14:20:50 -0400

    a

--- scripts/businessList/businessList.js
+++ scripts/businessList/businessList.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     function populateBusinessCards() {
         var $cardDeck = $('div.card-deck');
         
-        var $card = $("<a href='/RBC-NGI/pages/lacarnita.html'><div class='card'>" +
+        var $card = $("<a href='/RBC-NGI/pages/lacarnita.html' style='display: block; width:100%;'><div class='card'>" +
                         "<div class='card-background'></div>" +
                         "<div class='card-overlay'>" +
                             "<div class='promotionValue'>1<img src='../images/Shape%202-1.png' style='transform: translateY(-2px);'> / $10</div>" +
